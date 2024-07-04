d8d9e0342f6c9597db536fa740e58dbac831c78a
Marjan Nobakht <m.nobakht@servicecontrol-xe.com>
2024-07-04 12:04:03 +0200

    Move all files to root directory

--- celery_config.py
+++ celery_config.py
@@ -3,10 +3,10 @@ import os
 
 redis_url = os.getenv('REDIS_URL')
 
-celery_app = Celery('my_project', broker=redis_url, backend=redis_url)
+celery_app = Celery('tasks', broker=redis_url, backend=redis_url)
 
 celery_app.conf.update(
     result_expires=3600,
 )
 
-celery_app.autodiscover_tasks(['my_project'])
+celery_app.autodiscover_tasks(['.'])
